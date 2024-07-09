706dd478387ebd4877f941e0d88fb80eefe266fe
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-07-09 02:35:26 +0100

    cnx

--- connection.php
+++ connection.php
@@ -1,18 +1,32 @@
 <?php
-// Database connection parameters
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'fitness';
+//$host = 'localhost';
+//$username = 'root';
+//$password = '';
+//$database = 'fitness';
+//
+//$conn = mysqli_connect($host, $username, $password, $database);
+//
+//if (!$conn) {
+//    die('Connection failed: ' . mysqli_connect_error());
+//} else {
+////    echo 'Connection successful';
+//    return $conn;
+//}
 
-// Establishing the database connection
-$conn = mysqli_connect($host, $username, $password, $database);
 
-// Check if the connection was successful
-if (!$conn) {
-    die('Connection failed: ' . mysqli_connect_error());
-} else {
-    echo 'Connection successful';
-    return $conn;
-}
+    $host = 'localhost';
+    $dbname = 'fitness';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+        // Set PDO to throw exceptions on error
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        // Handle connection error
+        echo "Connection failed: " . $e->getMessage();
+        exit;
+    }
 ?>
+
